b782e20639c4075fb6426b95fc471877564c5d8c
MaciejSzcz <yetek.lol@gmail.com>
2020-03-01 23:19:51 +0100

    changed picture of location page

--- src/pages/lokalizacja.js
+++ src/pages/lokalizacja.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "../components/layout";
-import HeroPicture from "../images/mapka.jpg";
+import HeroPicture from "../images/mapka.png";
 
 const Lokalizacja = () => (
   <Layout>
