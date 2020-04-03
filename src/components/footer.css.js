0201fcf372111307cdab1baf04d1b0e04946dfcb
MaciejSzcz <yetek.lol@gmail.com>
2020-04-03 21:52:00 +0200

    fixed mobile css

--- src/components/footer.css.js
+++ src/components/footer.css.js
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
 
 export const Footer = styled.div`
-    height: 10vw;
     background-color: #3e3e3e;
     color: #ffffff;
-    padding: 2em 0 0 5vw;
+    padding: 2em 0 2em 5vw;
 `;
